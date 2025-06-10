acd1b1b99815a63a5d8bfa01c57df16ae8b0d011
addefj <adde.fj@gmail.com>
2025-06-10 14:24:07 +0200

    Part 2 defined methods in repositories

--- src/main/java/se/lexicon/workshopjpa/repository/AppUserRepository.java
+++ src/main/java/se/lexicon/workshopjpa/repository/AppUserRepository.java
@@ -3,7 +3,17 @@ package se.lexicon.workshopjpa.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import se.lexicon.workshopjpa.entity.AppUser;
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface AppUserRepository extends CrudRepository<AppUser, Integer> {
+    AppUser findByUsername(String username);
+
+    List<AppUser> findByRegDateIsBetween(LocalDate startDate, LocalDate endDate);
+
+    AppUser findByUserDetailsId(int id);
+
+    AppUser findByUserDetailsEmail(String email);
+
 }
--- src/main/java/se/lexicon/workshopjpa/repository/DetailsRepository.java
+++ src/main/java/se/lexicon/workshopjpa/repository/DetailsRepository.java
@@ -4,6 +4,12 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import se.lexicon.workshopjpa.entity.Details;
 
+import java.util.List;
+
 @Repository
 public interface DetailsRepository extends CrudRepository<Details, Integer> {
+    Details findByEmail(String email);
+    List<Details> findByNameContains(String name);
+    List<Details> findByNameIgnoreCase(String name);
+
 }
