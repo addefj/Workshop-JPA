75eff9d70b7160bf3634579bae626896b0665b82
addefj <adde.fj@gmail.com>
2025-06-16 16:10:09 +0200

    Part 3 added missing @Repository to repos

--- src/main/java/se/lexicon/workshopjpa/repository/BookLoanRepository.java
+++ src/main/java/se/lexicon/workshopjpa/repository/BookLoanRepository.java
@@ -1,11 +1,13 @@
 package se.lexicon.workshopjpa.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import se.lexicon.workshopjpa.entity.BookLoan;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Repository
 public interface BookLoanRepository extends CrudRepository<BookLoan, Integer> {
     List<BookLoan> findByBorrowerId(int id);
     BookLoan findByBookId(int id);
--- src/main/java/se/lexicon/workshopjpa/repository/BookRepository.java
+++ src/main/java/se/lexicon/workshopjpa/repository/BookRepository.java
@@ -1,10 +1,12 @@
 package se.lexicon.workshopjpa.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import se.lexicon.workshopjpa.entity.Book;
 
 import java.util.List;
 
+@Repository
 public interface BookRepository extends CrudRepository<Book, Integer> {
     Book findByIsbnIgnoreCase(String isbn);
     Book findByTitleContainsIgnoreCase(String title);
