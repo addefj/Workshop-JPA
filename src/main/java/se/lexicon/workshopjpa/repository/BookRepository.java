700dc3bb363b0fb675fb862ec3176b0938832790
addefj <adde.fj@gmail.com>
2025-06-16 14:43:27 +0200

    Part 3 added bookRepository and defined methods

---
+++ src/main/java/se/lexicon/workshopjpa/repository/BookRepository.java
@@ -0,0 +1,12 @@
+package se.lexicon.workshopjpa.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import se.lexicon.workshopjpa.entity.Book;
+
+import java.util.List;
+
+public interface BookRepository extends CrudRepository<Book, Integer> {
+    Book findByIsbnIgnoreCase(String isbn);
+    Book findByTitleContainsIgnoreCase(String title);
+    List<Book> findByMaxLoanDaysLessThan(int maxLoanDays);
+}
